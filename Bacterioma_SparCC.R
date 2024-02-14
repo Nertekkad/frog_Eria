101e5c6b6f8fbb8728cfccabf20f7af511885800
Nertekkad <61397302+Nertekkad@users.noreply.github.com>
2024-02-13 18:48:23 -0600

    Corrections... again

--- Bacterioma_SparCC.R
+++ Bacterioma_SparCC.R
@@ -59,47 +59,47 @@ library(mlBioNets)
 
 # Tadpole under treatment 1
 BTad_T1<-T_collapse(F, T_table = tax_bacter, O_table = BaTad_T1, names_level = "Genus")
-BTad_T1<-BTad_T1[, -which(colnames(BTad_T1) == "unidentified")]
+BTad_T1<-BTad_T1[, -which(colnames(BTad_T1) == "uncultured")]
 BTad_T1<-BTad_T1[, -which(is.na(colnames(BTad_T1)))]
 
 # Tadpole under treatment 2
 BTad_T2<-T_collapse(F, T_table = tax_bacter, O_table = BaTad_T2, names_level = "Genus")
-BTad_T2<-BTad_T2[, -which(colnames(BTad_T2) == "unidentified")]
+BTad_T2<-BTad_T2[, -which(colnames(BTad_T2) == "uncultured")]
 BTad_T2<-BTad_T2[, -which(is.na(colnames(BTad_T2)))]
 
 # Tadpole control
 BTad_Ctr<-T_collapse(F, T_table = tax_bacter, O_table = BaTad_Ctr, names_level = "Genus")
-BTad_Ctr<-BTad_Ctr[, -which(colnames(BTad_Ctr) == "unidentified")]
+BTad_Ctr<-BTad_Ctr[, -which(colnames(BTad_Ctr) == "uncultured")]
 BTad_Ctr<-BTad_Ctr[, -which(is.na(colnames(BTad_Ctr)))]
 
 # Metamorphic under treatment 1
 BMet_T1<-T_collapse(F, T_table = tax_bacter, O_table = BaMet_T1, names_level = "Genus")
-BMet_T1<-BMet_T1[, -which(colnames(BMet_T1) == "unidentified")]
+BMet_T1<-BMet_T1[, -which(colnames(BMet_T1) == "uncultured")]
 BMet_T1<-BMet_T1[, -which(is.na(colnames(BMet_T1)))]
 
 # Metamorphic under treatment 2
 BMet_T2<-T_collapse(F, T_table = tax_bacter, O_table = BaMet_T2, names_level = "Genus")
-BMet_T2<-BMet_T2[, -which(colnames(BMet_T2) == "unidentified")]
+BMet_T2<-BMet_T2[, -which(colnames(BMet_T2) == "uncultured")]
 BMet_T2<-BMet_T2[, -which(is.na(colnames(BMet_T2)))]
 
 # Metamorphic control
 BMet_Ctr<-T_collapse(F, T_table = tax_bacter, O_table = BaMet_Ctr, names_level = "Genus")
-BMet_Ctr<-BMet_Ctr[, -which(colnames(BMet_Ctr) == "unidentified")]
+BMet_Ctr<-BMet_Ctr[, -which(colnames(BMet_Ctr) == "uncultured")]
 BMet_Ctr<-BMet_Ctr[, -which(is.na(colnames(BMet_Ctr)))]
 
 # Sub-adult under treatment 1
 BAdl_T1<-T_collapse(F, T_table = tax_bacter, O_table = BaAdl_T1, names_level = "Genus")
-BAdl_T1<-BAdl_T1[, -which(colnames(BAdl_T1) == "unidentified")]
+BAdl_T1<-BAdl_T1[, -which(colnames(BAdl_T1) == "uncultured")]
 BAdl_T1<-BAdl_T1[, -which(is.na(colnames(BAdl_T1)))]
 
 # Sub-adult under treatment 2
 BAdl_T2<-T_collapse(F, T_table = tax_bacter, O_table = BaAdl_T2, names_level = "Genus")
-BAdl_T2<-BAdl_T2[, -which(colnames(BAdl_T2) == "unidentified")]
+BAdl_T2<-BAdl_T2[, -which(colnames(BAdl_T2) == "uncultured")]
 BAdl_T2<-BAdl_T2[, -which(is.na(colnames(BAdl_T2)))]
 
 # Sub-adult control
 BAdl_Ctr<-T_collapse(F, T_table = tax_bacter, O_table = BaAdl_T2, names_level = "Genus")
-BAdl_Ctr<-BAdl_Ctr[, -which(colnames(BAdl_Ctr) == "unidentified")]
+BAdl_Ctr<-BAdl_Ctr[, -which(colnames(BAdl_Ctr) == "uncultured")]
 BAdl_Ctr<-BAdl_Ctr[, -which(is.na(colnames(BAdl_Ctr)))]
 
 
@@ -127,7 +127,7 @@ ml_BAdl_sp<-readRDS("~/frog_Eria/ml_BAdl_sp.RDS")
 #### Multilayer networks ####
 
 unq<-unique(tax_bacter[,"Phylum"])
-unq<-unq[-c(which(unq == "unidentified"), which(is.na(unq)))]
+unq<-unq[-c(which(unq == "uncultured"), which(is.na(unq)))]
 colors <- sample(rainbow(100), length(unq))
 
 # Abundance tables list
@@ -248,7 +248,7 @@ ggbarplot(df_degree, x = "Phylum", y = "z_score",
           ggtheme = theme_minimal()) +
   theme(legend.position = "none")
 
-ggbarplot(df_vale, x = "Phylum", y = "log2fc",
+ggbarplot(df_degree, x = "Phylum", y = "log2fc",
           fill = "Phylum",
           color = "white",
           palette = colors,
