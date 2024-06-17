68504b8304c31f84d1cf0d5acd86fd1799aa1083
Nertekkad <61397302+Nertekkad@users.noreply.github.com>
2024-06-17 02:54:13 -0600

    Poincare plots

--- Early warnings analysis.R
+++ Early warnings analysis.R
@@ -15,8 +15,7 @@ tadID<-meta_bacterias$sample.id[which(meta_bacterias$Life_stage %in% "Tadpole")]
 metID<-meta_bacterias$sample.id[which(meta_bacterias$Life_stage %in% "Metamorphic")]
 adlID<-meta_bacterias$sample.id[which(meta_bacterias$Life_stage %in% "Sub-adult")]
 
-[which(colnames(bacterias) %in% adlID)]
-
+#### Richness plots ####
 
 richness<-function(ab_table){
   richness<-c()
@@ -129,7 +128,7 @@ ggbetweenstats(
   title = "Sub-adult"
 )
 
-
+#### Early warnings metrics ####
 
 # Shannon diversity
 Div_Shannon <- function(abundancias_ab){
@@ -242,51 +241,138 @@ plot(exp_ews, y_lab = "Abundances")
 
 
 
-mi_vector <- c(rnorm(2000, mean = 0, sd = 30), rnorm(2000, mean = 60, sd = 200))
-diferencias <- diff(mi_vector)
-plot(mi_vector[-length(mi_vector)], diferencias, type = "p", pch = 16,
-     col = "blue", xlab = "Observaciones", ylab = "Diferencias")
+
+#### Auto-correlation ####
+
+###### Separation of data by life stages type ######
+
+# Identify frog´s life stages
+life_stage<-unique(meta_bacterias$Life_stage)
+
+Tadpole<-meta_bacterias[which(meta_bacterias$Life_stage == life_stage[1]),]
+Metamorphic<-meta_bacterias[which(meta_bacterias$Life_stage == life_stage[2]),]
+Sub_adult<-meta_bacterias[which(meta_bacterias$Life_stage == life_stage[3]),]
+
+# Tadpole
+
+# Treatment identification
+Tad_T1<-Tadpole$sample.id[which(Tadpole$Treatment=="Treatment1")]
+Tad_T2<-Tadpole$sample.id[which(Tadpole$Treatment=="Treatment2")]
+Tad_Ctr<-Tadpole$sample.id[which(Tadpole$Treatment=="Control")]
+# Separation of data by treatment types
+BaTad_T1<-as.data.frame(bacterias[,which(colnames(bacterias) %in% Tad_T1)])
+BaTad_T2<-as.data.frame(bacterias[,which(colnames(bacterias) %in% Tad_T2)])
+BaTad_Ctr<-as.data.frame(bacterias[,which(colnames(bacterias) %in% Tad_Ctr)])
+
+# Metamorphic
+
+# Treatment identification
+Met_T1<-Metamorphic$sample.id[which(Metamorphic$Treatment=="Treatment1")]
+Met_T2<-Metamorphic$sample.id[which(Metamorphic$Treatment=="Treatment2")]
+Met_Ctr<-Metamorphic$sample.id[which(Metamorphic$Treatment=="Control")]
+# Separation of data by treatment types
+BaMet_T1<-as.data.frame(bacterias[,which(colnames(bacterias) %in% Met_T1)])
+BaMet_T2<-as.data.frame(bacterias[,which(colnames(bacterias) %in% Met_T2)])
+BaMet_Ctr<-as.data.frame(bacterias[,which(colnames(bacterias) %in% Met_Ctr)])
+
+# Sub-adult
+
+# Treatment identification
+Adl_T1<-Sub_adult$sample.id[which(Sub_adult$Treatment=="Treatment1")]
+Adl_T2<-Sub_adult$sample.id[which(Sub_adult$Treatment=="Treatment2")]
+Adl_Ctr<-Sub_adult$sample.id[which(Sub_adult$Treatment=="Control")]
+# Separation of data by treatment types
+BaAdl_T1<-as.data.frame(bacterias[,which(colnames(bacterias) %in% Adl_T1)])
+BaAdl_T2<-as.data.frame(bacterias[,which(colnames(bacterias) %in% Adl_T2)])
+BaAdl_Ctr<-as.data.frame(bacterias[,which(colnames(bacterias) %in% Adl_Ctr)])
+
+# Taxa table
+t1<-which(colnames(bacterias)=="Kingdom")
+t2<-which(colnames(bacterias)=="Species")
+tax_bacter<-as.data.frame(bacterias[,t1:t2])
+
+library(mlBioNets)
+
+# Tadpole under treatment 1
+BTad_T1<-T_collapse(F, T_table = tax_bacter, O_table = BaTad_T1, names_level = "Genus")
+BTad_T1<-BTad_T1[, -which(colnames(BTad_T1) == "uncultured")]
+BTad_T1<-BTad_T1[, -which(is.na(colnames(BTad_T1)))]
+
+# Tadpole under treatment 2
+BTad_T2<-T_collapse(F, T_table = tax_bacter, O_table = BaTad_T2, names_level = "Genus")
+BTad_T2<-BTad_T2[, -which(colnames(BTad_T2) == "uncultured")]
+BTad_T2<-BTad_T2[, -which(is.na(colnames(BTad_T2)))]
+
+# Tadpole control
+BTad_Ctr<-T_collapse(F, T_table = tax_bacter, O_table = BaTad_Ctr, names_level = "Genus")
+BTad_Ctr<-BTad_Ctr[, -which(colnames(BTad_Ctr) == "uncultured")]
+BTad_Ctr<-BTad_Ctr[, -which(is.na(colnames(BTad_Ctr)))]
+
+# Metamorphic under treatment 1
+BMet_T1<-T_collapse(F, T_table = tax_bacter, O_table = BaMet_T1, names_level = "Genus")
+BMet_T1<-BMet_T1[, -which(colnames(BMet_T1) == "uncultured")]
+BMet_T1<-BMet_T1[, -which(is.na(colnames(BMet_T1)))]
+
+# Metamorphic under treatment 2
+BMet_T2<-T_collapse(F, T_table = tax_bacter, O_table = BaMet_T2, names_level = "Genus")
+BMet_T2<-BMet_T2[, -which(colnames(BMet_T2) == "uncultured")]
+BMet_T2<-BMet_T2[, -which(is.na(colnames(BMet_T2)))]
+
+# Metamorphic control
+BMet_Ctr<-T_collapse(F, T_table = tax_bacter, O_table = BaMet_Ctr, names_level = "Genus")
+BMet_Ctr<-BMet_Ctr[, -which(colnames(BMet_Ctr) == "uncultured")]
+BMet_Ctr<-BMet_Ctr[, -which(is.na(colnames(BMet_Ctr)))]
+
+# Sub-adult under treatment 1
+BAdl_T1<-T_collapse(F, T_table = tax_bacter, O_table = BaAdl_T1, names_level = "Genus")
+BAdl_T1<-BAdl_T1[, -which(colnames(BAdl_T1) == "uncultured")]
+BAdl_T1<-BAdl_T1[, -which(is.na(colnames(BAdl_T1)))]
+
+# Sub-adult under treatment 2
+BAdl_T2<-T_collapse(F, T_table = tax_bacter, O_table = BaAdl_T2, names_level = "Genus")
+BAdl_T2<-BAdl_T2[, -which(colnames(BAdl_T2) == "uncultured")]
+BAdl_T2<-BAdl_T2[, -which(is.na(colnames(BAdl_T2)))]
+
+# Sub-adult control
+BAdl_Ctr<-T_collapse(F, T_table = tax_bacter, O_table = BaAdl_T2, names_level = "Genus")
+BAdl_Ctr<-BAdl_Ctr[, -which(colnames(BAdl_Ctr) == "uncultured")]
+BAdl_Ctr<-BAdl_Ctr[, -which(is.na(colnames(BAdl_Ctr)))]
 
 
+poincare_plot<-function(ab_table, title){
+  require(ggplot2)
+  require(data.table)
+  # Create vector
+  data <- ab_table_div(ab_table, "shannon")
+  data<-datos[-which(data == 0)]
+  # Quantify the dots for Poincaré plot
+  poincare_data <- data.frame(
+    x = data[-length(data)], 
+    y = data[-1]
+  )
+  # Create plot
+  p <- ggplot(poincare_data, aes(x = x, y = y)) +
+    geom_point() +
+    theme_minimal()
+  # Add ellipse
+  p <- p + stat_ellipse(type = "t", level = 0.95) + labs(x = "RR_n", y = "RR_n+1", title = title)
+  return(p)
+}
 
+pc_Tad_Ctr<-poincare_plot(BTad_Ctr, "Tadpole's control")
+pc_Tad_T1<-poincare_plot(BTad_T1, "Tadpole's treatment 1")
+pc_Tad_T2<-poincare_plot(BTad_T2, "Tadpole's treatment 2")
 
+pc_Met_Ctr<-poincare_plot(BMet_Ctr, "Metamorphic's control")
+pc_Met_T1<-poincare_plot(BMet_T1, "Metamorphoic's treatment 1")
+pc_Met_T2<-poincare_plot(BMet_T2, "Metamorphic's treatment 2")
 
+pc_Adl_Ctr<-poincare_plot(BAdl_Ctr, "Sub-adult's control")
+pc_Adl_T1<-poincare_plot(BAdl_T1, "Sub-adult's treatment 1")
+pc_Adl_T2<-poincare_plot(BAdl_T2, "Sub-adult's treatment 2")
 
-
-
-
-
-
-
-
-
-
-
-
-library(ggplot2)
-library(dplyr)
-
-# Simulación de datos (reemplaza esto con tus datos reales)
-set.seed(123)
-rr_intervals <- rnorm(500, mean = 1000, sd = 50)
-
-# Calcula los intervalos RR consecutivos
-rr_diff <- diff(rr_intervals)
-
-# Crea un data frame con los intervalos RR y sus diferencias
-poincare_data <- data.frame(RRn = rr_intervals[-length(rr_intervals)],
-                            RRn1 = rr_intervals[-1],
-                            RR_diff = rr_diff)
-
-# Gráfico de Poincaré
-ggplot(poincare_data, aes(x = RRn, y = RRn1)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE, color = "blue") +
-  labs(x = "RRn (ms)", y = "RRn+1 (ms)",
-       title = "Diagrama de Poincaré",
-       subtitle = "SD1 y SD2 se derivan de esta elipse") +
-  theme_minimal()
-
-
-
-
+library(gridExtra)
+grid.arrange(pc_Tad_Ctr, pc_Tad_T1, pc_Tad_T2,
+             pc_Met_Ctr, pc_Met_T1, pc_Met_T2,
+             pc_Adl_Ctr, pc_Adl_T1, pc_Adl_T2,
+             ncol = 3)
